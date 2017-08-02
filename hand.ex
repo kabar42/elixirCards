9e54c4c7af112008832e43653614d42bb022fabd
Kent Barnett <barnettkent@gmail.com>
2017-08-01 19:30:07 -0500

    Hey, it works now

--- hand.ex
+++ hand.ex
@@ -11,8 +11,7 @@ defmodule Hand do
   end
 
   def generateHands!([c | deck], [], handList) do
-    generateHands!(deck, [c], handList)
-    generateHands!(deck, [], handList)
+    generateHands!(deck, [c], handList) ++ generateHands!(deck, [], handList)
   end
 
   def generateHands!([c | deck], hand, handList) do
@@ -20,8 +19,7 @@ defmodule Hand do
       length(hand) >= 5 ->
         [hand | handList]
       true ->
-        generateHands!(deck, [c | hand], handList)
-        generateHands!(deck, hand, handList)
+        generateHands!(deck, [c | hand], handList) ++ generateHands!(deck, hand, handList)
     end
   end
 end
